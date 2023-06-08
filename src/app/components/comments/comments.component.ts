e941fe427fc572e2a1884615fecc8a969709378a
mathieusi38 <mathieusi38@gmail.com>
2023-06-08 22:16:22 +0200

    Update service comments

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -5,6 +5,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { Firestore, collection, addDoc, getDocs, getDoc  } from '@angular/fire/firestore';
 //import {AngularFirestore } from "@angular/fire/compat/firestore";
 import { CommentsService } from 'src/app/services/comments.services';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -14,33 +15,67 @@ import { CommentsService } from 'src/app/services/comments.services';
 })
 export class CommentsComponent {
 
-  comments: any;
+  comments!: Observable<any>;
   inProgress: Comment[] = [];
   done: Comment[] = [];
   console = console;
+  displayedColumns: string[] = ['title', 'description', 'edit', 'remove'];
+  dataComment = {
+    title: '',
+    description: '',
+  };
 
   constructor(private dialog: MatDialog, private store: Firestore, private CommentsService: CommentsService) {
+  }
+
+  ngOnInit(): void {
     this.comments = this.CommentsService.getComments();
   }
 
   newComment(): void {
+
     const dialogRef = this.dialog.open(CommentDialogComponent, {
       width: '270px',
       data: {
       },
-    });
+    }); 
     dialogRef
     .afterClosed()
     .subscribe((result) => {
       if (!result) {
         return;
       }
-      addDoc(collection(this.store, 'comments'), result.comments);
+      this.CommentsService.addComment(result.comments);
     });
   }
 
   deleteComment(id: string) {
     this.CommentsService.deleteComment(id);
+  }
+
+  updateComments(id: string) {
+
+    this.CommentsService.getComment(id).then((commentData) => {
+      const dialogRef = this.dialog.open(CommentDialogComponent, {
+        width: '270px',
+        data: {
+          title: commentData['title'],
+          description: commentData['description'],
+        },
+      }); 
+      dialogRef
+      .afterClosed()
+      .subscribe((result) => {
+        if (!result) {
+          return;
+        }
+        this.CommentsService.updateComment(id ,result.comments);
+      });
+      
+    }).catch((error) => {
+      throw new Error("no comment find");
+    });
+
   }
 
 
--- src/app/services/comments.services.ts
+++ src/app/services/comments.services.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { Firestore, getDoc, getDocs, collection, doc, deleteDoc } from '@angular/fire/firestore';
+import { Firestore, getDoc, getDocs, collection, doc, deleteDoc, addDoc, collectionData, updateDoc } from '@angular/fire/firestore';
 import { Comment } from '../models/interfaces';
 import {Observable} from "rxjs";
 
@@ -14,24 +14,41 @@ export class CommentsService {
 
   }
 
+  addComment(newComment: Comment) {
+    const collectionInstance = collection(this.store, 'comments');
+    addDoc(collectionInstance, newComment)
+  }
 
   getComments() {
-    let comments: Comment[] = [];
-   // return getObservable<Comment[]>(collection(this.store, 'comments'));
-    getDocs(collection(this.store, 'comments')).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            comments.push(doc.data() as Comment);
-            comments[comments.length-1].id = doc.id;
+    const comments = collection(this.store, 'comments');
+    collectionData(comments, { idField: 'id' }).subscribe(value => {
+    }) 
 
-        });
-     });
-
-    return comments;
-
+    return collectionData(comments, { idField: 'id' });
     }
 
     deleteComment(id: string) {
         //Delete a document with a known ID
         deleteDoc(doc(this.store, 'comments', id));
     }
+
+    getComment(id: string) {
+      const docInstance = doc(this.store, 'comments', id);
+      return getDoc(docInstance).then((docSnapshot) => {
+        if (docSnapshot.exists()) {
+          return docSnapshot.data();
+        } else {
+          throw new Error("no comment find");
+        }
+      })
+    }
+
+    updateComment(id : string, updateComment : Comment) {
+      const docInstance = doc(this.store, 'comments', id);
+      updateDoc(docInstance, {
+        title: updateComment.title,
+        description: updateComment.description,
+      })
+    }
+
 }
